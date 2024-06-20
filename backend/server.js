0552406d0f097cb0c9b2a37f67d119b3fa154072
Anusha Syeda <anoshasyeda1999@gmail.com>
2024-06-20 21:52:23 +0500

    chore: Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,78 @@
-const express = require("express");
+// const express = require("express");
+// const puppeteer = require("puppeteer");
+
+// const app = express();
+
+// async function fetchUrls() {
+//   try {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto("https://news.ycombinator.com/");
+//     let urls = await page.evaluate(() => {
+//       let results = [];
+//       let items = document.querySelectorAll("a");
+//       items.forEach((item) => {
+//         results.push({
+//           url: item.getAttribute("href"),
+//           text: item.innerText,
+//         });
+//       });
+//       return results;
+//     });
+//     await browser.close();
+//     return urls;
+//   } catch (e) {
+//     throw e;
+//   }
+// }
+
+// app.get("/fetch-urls", async (req, res) => {
+//   try {
+//     const urls = await fetchUrls();
+//     res.json(urls);
+//   } catch (error) {
+//     res.status(500).send({ error: error.message });
+//   }
+// });
+
+// const PORT = process.env.PORT || 5173;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 const puppeteer = require("puppeteer");
+// Launch the browser and open a new blank page
 
-const app = express();
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
 
-async function fetchUrls() {
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto("https://news.ycombinator.com/");
-    let urls = await page.evaluate(() => {
-      let results = [];
-      let items = document.querySelectorAll("a");
-      items.forEach((item) => {
-        results.push({
-          url: item.getAttribute("href"),
-          text: item.innerText,
-        });
-      });
-      return results;
-    });
-    await browser.close();
-    return urls;
-  } catch (e) {
-    throw e;
-  }
-}
+  // Navigate the page to a URL.
+  await page.goto("https://insigh.to/");
 
-app.get("/fetch-urls", async (req, res) => {
-  try {
-    const urls = await fetchUrls();
-    res.json(urls);
-  } catch (error) {
-    res.status(500).send({ error: error.message });
-  }
-});
+  // Set screen size.
+  await page.setViewport({ width: 1280, height: 1624 });
 
-const PORT = process.env.PORT || 5173;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  // Type into search box.
+  // await page.type(".devsite-search-field", "automate beyond recorder");
+  await page.screenshot({ path: "example.png" });
+
+  // Wait and click on first result.
+  // const searchResultSelector = ".devsite-result-item-link";
+  // await page.waitForSelector(searchResultSelector);
+  // await page.click(searchResultSelector);
+
+  // Locate the full title with a unique string.
+  // const textSelector = await page.waitForSelector(
+  //   "text/Customize and automate"
+  // );
+  // const fullTitle = await textSelector?.evaluate((el) => el.textContent);
+
+  // // Print the full title.
+  // console.log('The title of this blog post is "%s".', fullTitle);
+  await browser.close();
+})();
+
+// https://www.websitecarbon.com/
+// https://ecograder.com/
+// https://news.ycombinator.com/
+// https://insigh.to/
