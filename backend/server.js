5a51e36797d5c5491d4b5c2e4bd4f348304c69aa
Anusha Syeda <anoshasyeda1999@gmail.com>
2024-06-21 20:20:07 +0500

    chore

--- backend/server.js
+++ backend/server.js
@@ -1,78 +1,16 @@
-// const express = require("express");
-// const puppeteer = require("puppeteer");
-
-// const app = express();
-
-// async function fetchUrls() {
-//   try {
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
-//     await page.goto("https://news.ycombinator.com/");
-//     let urls = await page.evaluate(() => {
-//       let results = [];
-//       let items = document.querySelectorAll("a");
-//       items.forEach((item) => {
-//         results.push({
-//           url: item.getAttribute("href"),
-//           text: item.innerText,
-//         });
-//       });
-//       return results;
-//     });
-//     await browser.close();
-//     return urls;
-//   } catch (e) {
-//     throw e;
-//   }
-// }
-
-// app.get("/fetch-urls", async (req, res) => {
-//   try {
-//     const urls = await fetchUrls();
-//     res.json(urls);
-//   } catch (error) {
-//     res.status(500).send({ error: error.message });
-//   }
-// });
-
-// const PORT = process.env.PORT || 5173;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
 const puppeteer = require("puppeteer");
 // Launch the browser and open a new blank page
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
 
   // Navigate the page to a URL.
-  await page.goto("https://insigh.to/");
+  await page.goto("https://www.vocabulary.com/lists/52473");
 
   // Set screen size.
   await page.setViewport({ width: 1280, height: 1624 });
 
-  // Type into search box.
-  // await page.type(".devsite-search-field", "automate beyond recorder");
-  await page.screenshot({ path: "example.png" });
-
-  // Wait and click on first result.
-  // const searchResultSelector = ".devsite-result-item-link";
-  // await page.waitForSelector(searchResultSelector);
-  // await page.click(searchResultSelector);
-
-  // Locate the full title with a unique string.
-  // const textSelector = await page.waitForSelector(
-  //   "text/Customize and automate"
-  // );
-  // const fullTitle = await textSelector?.evaluate((el) => el.textContent);
-
-  // // Print the full title.
-  // console.log('The title of this blog post is "%s".', fullTitle);
+  await page.screenshot({ path: "example.png", fullPage: true });
   await browser.close();
 })();
-
-// https://www.websitecarbon.com/
-// https://ecograder.com/
-// https://news.ycombinator.com/
-// https://insigh.to/
